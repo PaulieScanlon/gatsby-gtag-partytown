0f07689f2884d64178c800eb957fc295f9e0ddd2
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-23 11:24:28 +0000

    feat: add Partytown 🎉

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,5 @@
 module.exports = {
   siteMetadata: {
     title: 'gatsby-gtag-partytown'
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-plugin-google-gtag',
-      options: {
-        trackingIds: ['G-C60YCNF3ND'],
-        pluginConfig: {
-          head: true
-        }
-      }
-    }
-  ]
+  }
 };
